2f008b3c99a2e7818afa64aa9ce3f84d1e5e2644
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-10-30 00:49:21 +0600

    Update BasicJavascript.js

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -130,3 +130,21 @@ var lastName = "Lovelace";
 // Only change code below this line
 
 lastNameLength = lastName.length; */
+
+/* Use Bracket Notation to Find the First Character in a String
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line */
+
+
+/* Understand String Immutability
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+ */
