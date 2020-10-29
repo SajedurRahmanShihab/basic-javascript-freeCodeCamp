4104c0ab0e8bf8dd19348e7e74958048e6300596
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-10-30 00:21:33 +0600

    Update BasicJavascript.js

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -121,3 +121,12 @@ var myStr = "My name is "+ myName + " and I am well!";
 var myStr = "Learning to code is ";
 mystr += someAdjective;
  */
+
+/* Find the Length of a String
+ // Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line
+
+lastNameLength = lastName.length; */
