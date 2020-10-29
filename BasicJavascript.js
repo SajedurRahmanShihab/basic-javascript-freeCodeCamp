d7b0c48b38f404191185a180b7e2865d6318047b
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-10-30 00:51:50 +0600

    Update BasicJavascript.js

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -156,3 +156,11 @@ var lastName = "Lovelace";
 // Only change code below this line
 var thirdLetterOfLastName = lastName[2]; // Change this line
  */
+
+/* Use Bracket Notation to Find the Last Character in a String
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var lastLetterOfLastName = lastName[lastname.length-1]; // Change this line
+ */
