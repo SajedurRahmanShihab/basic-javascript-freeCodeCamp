6f0df68efaba143041fb0b9952124f18345597c5
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-10-29 22:40:00 +0600

    Update BasicJavascript.js

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -48,3 +48,13 @@ titleCaseOver = 9000; */
 
 // Subtract One Number from Another with JavaScript
 /* var difference = 45 - 33; */
+
+// Compound Assignment With Augmented Addition
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
