0ab81b729af267d2ef13e965b6bc5e90c4d12851
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-10-29 22:18:18 +0600

    Assigning the Value of One Variable to Another

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -5,5 +5,15 @@ multi-line comment */
 
 // var myName;
 
+// Storing Values with the Assignment Operator
 /*var a;
 a = 7; */
+
+// Assigning the Value of One Variable to Another
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+b = a;
