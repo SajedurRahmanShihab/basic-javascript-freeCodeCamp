9e43de7c962c7b79b0bea28aab9d1ea7fd48bbf6
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-10-30 19:10:39 +0600

    Update BasicJavascript.js

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -727,3 +727,33 @@ function caseInSwitch(val) {
 }
 
 caseInSwitch(1); */
+
+
+
+
+/* Adding a Default Option in Switch Statements
+
+function switchOfStuff(val) {
+    var answer = "";
+    // Only change code below this line
+    switch (val) {
+        case "a":
+            return "apple";
+            break;
+        case "b":
+            return "bird";
+            break;
+        case "c":
+            return "cat";
+            break;
+        default:
+            return "stuff";
+            break;
+    }
+
+
+    // Only change code above this line
+    return answer;
+}
+
+switchOfStuff(1); */
