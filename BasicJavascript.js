99f9b9cd9051874547e6e03e1d2561755a4b5cbe
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-10-31 00:06:30 +0600

    Update BasicJavascript.js

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -1127,3 +1127,41 @@ var myPlants = [
   
   var secondTree = myPlants[1].list[1]; // Change this line
    */
+
+
+
+
+/* Record CollectionPassed
+  var collection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
+    }
+  };
+  
+  // Only change code below this line
+  function updateRecords(object, id, prop, value) {
+    if (value === '') delete object[id][prop];
+    else if (prop === 'tracks') {
+      object[id][prop] = object[id][prop] || [];
+      object[id][prop].push(value);
+    } else {
+      object[id][prop] = value;
+    }
+  
+    return object;
+}
+   */
