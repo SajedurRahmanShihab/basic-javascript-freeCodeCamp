eede06368d8c87ff135c0ab25929245dfe47ac33
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-10-30 23:45:25 +0600

    Create test.js

---
+++ test.js
@@ -0,0 +1,15 @@
+var myself = {
+    "name": "Sajedur Rahman",
+    "Education": {
+        "SSC": "Pabna Collectorate Public School And College",
+        "HSC": "Bogura Cantonment Public School And College",
+        "B.Sc": "Daffodil Internation University"
+    },
+    "Family": {
+        "Parents": {
+            "father": "Mohammad Ali Shohel",
+            "mother": "Sharmin Begum",
+            "sister": "Nur-a-nahrin Mim"
+        }
+    }
+}
