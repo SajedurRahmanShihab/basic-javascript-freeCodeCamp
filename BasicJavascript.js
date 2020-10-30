1ebd98573e7113a02fd08a3a4069a4b13b8c2782
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-10-30 17:13:09 +0600

    Update BasicJavascript.js

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -358,3 +358,22 @@ function processArg(num) {
 
 // Only change code below this line
 processed = processArg(7); */
+
+
+/* Stand in Line
+function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item);
+    return arr.shift();
+    // Only change code above this line
+    
+  
+  }
+  
+  // Setup
+  var testArr = [1,2,3,4,5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr)); */
