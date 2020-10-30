53e93d79c693cb48dc6b19b9b422ba1facf001e4
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-10-31 00:08:36 +0600

    Update BasicJavascript.js

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -1178,3 +1178,13 @@ while(i>=0){
     myArray.push(i);
     i--;
 } */
+
+
+/* Iterate with JavaScript For Loops
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for(var i = 1; i<=5; i++){
+    myArray.push(i);
+} */
