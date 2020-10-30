c83df9b8b4c2ac3a0e21157628f761e5eaf6ee38
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-10-30 14:05:11 +0600

    Update BasicJavascript.js

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -303,3 +303,19 @@ function myLocalScope() {
   // Run and check the console
   // myVar is not defined outside of myLocalScope
   console.log('outside myLocalScope', myVar); */
+
+
+/*  Global vs. Local Scope in Functions
+  // Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+
+ var outerWear = "sweater";
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit(); */
