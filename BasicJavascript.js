08ba277996e4f7408b3480c4e13f4ca00162f97a
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-10-30 14:02:04 +0600

    Update BasicJavascript.js

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -289,3 +289,17 @@ function fun2() {
   console.log(output);
 }
  */
+
+/* Local Scope and Functions
+function myLocalScope() {
+    'use strict';
+  
+    // Only change code below this line
+    var myVar;
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar); */
