aa0ea2df1bf8b8fc23c18eac13b0a4adf9551e90
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-11-01 02:14:40 +0600

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -100,3 +100,11 @@ console.log(myConcat([1, 2], [3, 4, 5])); */
 // Only change code below this line
 const increment = (number, value = 1) => number + value;
 // Only change code above this line */
+
+
+/* Use the Rest Parameter with Function Parameters
+const sum = (...args) => {
+    const args = [x, y, z];
+    return args.reduce((a, b) => a + b, 0);
+  }
+   */
