41147238a27837cdfeb2d2becb9d2c568a4bc7fa
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-10-31 19:27:32 +0600

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -45,3 +45,20 @@ function checkScope() {
   printManyTimes("freeCodeCamp");
   
    */
+
+
+
+/*   Mutate an Array Declared with const
+  const s = [5, 7, 2];
+  function editInPlace() {
+    'use strict';
+    // Only change code below this line
+  
+    // Using s = [2, 5, 7] would be invalid
+   s[0] = 2;
+   s[1] = 5;
+   s[2] = 7;
+    // Only change code above this line
+  }
+  editInPlace();
+   */
