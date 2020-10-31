afefd11938bbb1e588a1e41d9e7936198edf3803
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-10-31 19:23:05 +0600

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -11,3 +11,37 @@ function catTalk() {
 }
 catTalk();
  */
+
+
+/* Compare Scopes of the var and let Keywords
+function checkScope() {
+  
+    let i = 'function scope';
+    if (true) {
+     let i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+  }
+   */
+
+
+
+/*   Declare a Read-Only Variable with the const Keyword
+  function printManyTimes(str) {
+    "use strict";
+  
+    // Only change code below this line
+  
+    const SENTENCE = str + " is cool!";
+    for (let i = 0; i < str.length; i+=2) {
+      console.log(SENTENCE);
+    }
+  
+    // Only change code above this line
+  
+  }
+  printManyTimes("freeCodeCamp");
+  
+   */
