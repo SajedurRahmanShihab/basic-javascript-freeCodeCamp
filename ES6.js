489cca4d4210362d2d962517476d829150d8c6c0
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-10-31 19:31:28 +0600

    Update ES6.js

--- ES6.js
+++ ES6.js
@@ -62,3 +62,23 @@ function checkScope() {
   }
   editInPlace();
    */
+
+
+/*   Prevent Object Mutation
+  function freezeObj() {
+    'use strict';
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
+    // Only change code below this line
+  Object.freeze(MATH_CONSTANTS.PI);
+  
+    // Only change code above this line
+    try {
+      MATH_CONSTANTS.PI = 3.14;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj(); */
